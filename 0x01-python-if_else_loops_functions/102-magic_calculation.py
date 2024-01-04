8203f8e4e5e21a8847df4437369026fab126b0ff
Rita086 <gathogoritawanjiru@gmail.com>
2024-01-04 14:45:54 -0800

    Sevententh file commit

--- 0x01-python-if_else_loops_functions/102-magic_calculation.py
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def magic_calculation(k, l, m):
     if k < l:
-        return (m)
+    return (m)
     if m > l:
-        return (k + l)
+    return (k + l)
     return (k*l - m)
