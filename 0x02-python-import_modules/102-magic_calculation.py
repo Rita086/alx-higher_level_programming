bfca6a2717f22bd4d4b2367ed428d2b2aca8c229
Rita086 <gathogoritawanjiru@gmail.com>
2024-01-04 15:27:38 -0800

    Nineth commit

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
 
 
-def magic_calculation(a, b):
+def magic_calculation(q ,r):
     """Match bytecode provided by Holberton School."""
     from magic_calculation_102 import add, sub
 
-    if a < b:
-        c = add(a, b)
-        for i in range(4, 6):
-            c = add(c, i)
-        return (c)
+    if q < r:
+        s = add(q, r)
+        for j in range(4, 6):
+            s = add(s, j)
+        return (s)
 
     else:
-        return(sub(a, b))
+        return(sub(q, r))
