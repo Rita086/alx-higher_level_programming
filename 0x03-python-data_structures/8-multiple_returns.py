6c2e9d48c9b93815bede11af76327497640ce611
Rita086 <gathogoritawanjiru@gmail.com>
2024-01-08 11:07:42 -0800

    This file 8-multiple_returns.py

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,17 +1,10 @@
 #!/usr/bin/python3
 
 def multiple_returns(sentence):
-
     if not sentence:
-
         sentence = None
-
-    if sentence:
-
-        sent_len = len(sentence)
-
-    else:
-
-        sen_len = 0
-
-    return (sent_len, sentence if not sentence else sentence[:1])
+        if sentence:
+            sent_len = len(sentence)
+        else:
+            sen_len = 0
+            return (sent_len, sentence if not sentence else sentence[:1])
