a423b6672dbb12bdff7a22b77616bbbb1c5b72fe
Rita086 <gathogoritawanjiru@gmail.com>
2024-01-10 04:55:38 -0800

    This file 2-uniq_add.py

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -2,9 +2,9 @@
 
 def multiple_returns(sentence):
     if not sentence:
-        sentence = None
-        if sentence:
-            sent_len = len(sentence)
-        else:
-            sen_len = 0
-            return (sent_len, sentence if not sentence else sentence[:1])
+    sentence = None
+    if sentence:
+    sent_len = len(sentence)
+    else:
+    sen_len = 0
+    return (sent_len, sentence if not sentence else sentence[:1])
