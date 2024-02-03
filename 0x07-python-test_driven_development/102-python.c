b4c51033e4caedbaffb497ea455b906fd5c7013b
Rita086 <gathogoritawanjiru@gmail.com>
2024-02-02 17:38:17 -0800

    File 102-python.c

--- 0x07-python-test_driven_development/102-python.c
+++ 0x07-python-test_driven_development/102-python.c
@@ -6,7 +6,7 @@
  */
 void print_python_string(PyObject *p)
 {
-	long int length;
+	long int len;
 
 	fflush(stdout);
 
@@ -17,12 +17,12 @@ void print_python_string(PyObject *p)
 		return;
 	}
        
-	length = ((PyASCIIObject *)(p))->length;
+	len = ((PyASCIIObject *)(p))->len;
 
 	if (PyUnicode_IS_COMPACT_ASCII(p))
 		printf("  type: compact ascii\n");
 	else
 		printf("  type: compact unicode object\n");
-	printf("  length: %ld\n", length);
-	printf("  value: %ls\n", PyUnicode_AsWideCharString(p, &length));
+	printf("  length: %ld\n", len);
+	printf("  value: %ls\n", PyUnicode_AsWideCharString(p, &len));
 }
